e54dd3f0f8bb78f4391776c9a30c8e5920f47336
pcodesdev <njugunapm23@gmail.com>
2023-11-27 22:54:35 +0300

    have designed a form in react and prevented the default loading behaviour

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -4,11 +4,26 @@ import './form.css'
 import plan from './../../assets/map-pin-dynamic-gradient.png'
 
 function Form() {
+  // form submit
+  function handleSubmit(e){
+e.preventDefault()
+console.log(e)
+  }
   return (
-    <div>
-        <h3>What are your plans for today? <span><img className='icon' src={plan} alt="plan-for-the-day" /></span></h3>
-    </div>
+    <form className='form_add-item' onSubmit={handleSubmit}>
+      <div className="form_add-inputs">
+        <h3>What are your plans for today? <span><img className='form-icon' src={plan} alt="plan-for-the-day" /></span></h3>
+        <select className='form_add-select' name="" id="">
+          {/* generate a number of options from 1 to 20 */}
+          {Array.from({length: 20}, (_,i) => i + 1).map((num) => (<option value={num} key={num}>{num}</option>))}
+        </select>
+        <input className='form_add-input' type="text" placeholder='Task...' />
+        <button className='form_add-btn'>Add</button>
+        </div>
+    </form>
   )
 }
 
 export default Form
+
+// Todo: Controlled Elements
--- src/components/tasks/Item.jsx
+++ src/components/tasks/Item.jsx
@@ -1,10 +1,12 @@
 import './task.css'
 
+import trash from './../../assets/trash-can-dynamic-gradient.png'
+
 function Item({item}) {
   return (
     <div className='list_items'>
-        <li key={item.id} ><span style={item.packed ?{textDecoration: 'line-through'}:{}}>{item.quantity} {item.description}</span>
-        <button>❌</button>
+        <li key={item.id} className='item_list'><span style={item.packed ?{textDecoration: 'line-through'}:{}}>{item.quantity} {item.description}</span>
+        <button className='close-btn'><img src={trash} alt="trash can" /></button>
         </li>
         </div>
   )
