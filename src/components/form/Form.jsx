fc4be70ccc23206236af009f30616927a8570b96
pcodesdev <njugunapm23@gmail.com>
2023-11-28 22:55:29 +0300

    have added controlled stated to the form input

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,22 +2,41 @@ import './form.css'
 
 
 import plan from './../../assets/map-pin-dynamic-gradient.png'
+import { useState } from 'react'
 
 function Form() {
+
+  const [description, setDescription] = useState("")
+  const [quantity, setQuantity] = useState(1)
   // form submit
   function handleSubmit(e){
 e.preventDefault()
-console.log(e)
+// only submit the form when complete
+if(!description) return
+
+const newTask = {
+  description,
+  quantity,
+  done: false,
+  id: Date.now(),
+}
+console.log(newTask)
+
+setDescription('')
+setQuantity(1)
   }
   return (
+
     <form className='form_add-item' onSubmit={handleSubmit}>
       <div className="form_add-inputs">
         <h3>What are your plans for today? <span><img className='form-icon' src={plan} alt="plan-for-the-day" /></span></h3>
-        <select className='form_add-select' name="" id="">
+        <select className='form_add-select' name="" id="" value={quantity} 
+        // change the state 
+        onChange={(e)=>setQuantity(Number(e.target.value))}>
           {/* generate a number of options from 1 to 20 */}
           {Array.from({length: 20}, (_,i) => i + 1).map((num) => (<option value={num} key={num}>{num}</option>))}
         </select>
-        <input className='form_add-input' type="text" placeholder='Task...' />
+        <input className='form_add-input' type="text" placeholder='Task...' value={description} onChange={(e) => setDescription(e.target.value)}/>
         <button className='form_add-btn'>Add</button>
         </div>
     </form>
@@ -26,4 +45,4 @@ console.log(e)
 
 export default Form
 
-// Todo: Controlled Elements
+// Todo: STATE, EVENTS, AND FORMS
